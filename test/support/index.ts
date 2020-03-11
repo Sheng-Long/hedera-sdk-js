111950d1e008f37c249a2e93c1077f96d85d803c
Tim <tim@launchbadge.com>
2020-03-10 19:15:33 -0700

    first attempt, might redo this whole thing

---
+++ test/support/index.ts
@@ -0,0 +1,5 @@
+// setup expect imports
+import * as _expect from "expect";
+const expect = _expect as any as typeof _expect.default;
+import * as _mocha_each from "mocha-each";
+const forEach = _mocha_each as any as typeof _mocha_each.default;
